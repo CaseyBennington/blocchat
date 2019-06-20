5da185e2f37ac4c147b5e353f4834c7d6d7a40df
Casey Bennington <cbennington@g2ss.com>
2019-06-19 17:42:05 -0700

    Completed list-messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import './App.css';
 import RoomList from './RoomList/RoomList';
+import Messages from './Messages/Messages';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -20,23 +21,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      activeRoom: null,
+      user: null
     };
   }
   
+  setUser(user) {
+    this.setState({user: user});
+  }
+
+  setRoom(room) {
+    this.setState({activeRoom: room});
+  }
   
   render() {
     return (
       <div className="App">
         <nav id="main">
+        
         </nav>
 
         <aside id="sidebar">
           <div id="logo"></div>
           <h1 id="wordmark">Tomato</h1>
-          <RoomList firebase={firebase} />
+          <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setRoom={this.setRoom.bind(this)} user={this.state.user} />
         </aside>
 
+        <Messages firebase={firebase} activeRoom={this.state.activeRoom} user={this.state.user} />
       </div>
     );
   }
